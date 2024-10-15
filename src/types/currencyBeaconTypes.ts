99cf21242306e937635682b3a5fd2b082fc2d8b0
Jakub Brambor <kuba.brambor@gmail.com>
2024-10-15 20:30:21 +0200

    add type

--- src/types/currencyBeaconTypes.ts
+++ src/types/currencyBeaconTypes.ts
@@ -3,6 +3,12 @@ export interface Currency {
   name: string
   short_code: string
   code: string
+  precision: number
+  subunit: number
+  symbol: string
+  symbol_first: boolean
+  decimal_mark: string
+  thousands_separator: string
 }
 
 export interface Meta {
@@ -13,5 +19,19 @@ export interface Meta {
 export interface CurrenciesResponse {
   meta: Meta
   response: Currency[]
+  [key: string]: unknown
+}
+
+export interface ConvertedValue {
+  amount: number
+  date: string
+  from: string
+  timestamp: number
+  to: string
+  value: number
+}
+export interface ConvertResponse {
+  meta: Meta
+  response: ConvertedValue
   [key: string]: unknown
 }
