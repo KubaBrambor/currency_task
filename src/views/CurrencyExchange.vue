934f4a919ef2e265b662993be42beb2bddd45ef7
Jakub Brambor <kuba.brambor@gmail.com>
2024-10-15 17:40:48 +0200

    implement SelectElement with mock data

--- src/views/CurrencyExchange.vue
+++ src/views/CurrencyExchange.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts"></script>
-
 <template>
   <main>
-    <TheWelcome />
+    <SelectElement v-model="selectedCurrency" :options="testArr" />
   </main>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import SelectElement from '@/components/shared/SelectElement.vue'
+
+const testArr = [
+  { id: 1, name: 'kuba', short_code: 'KU', code: 'KubaB' },
+  { id: 2, name: 'mark', short_code: 'MK', code: 'MarkV' },
+]
+const selectedCurrency = ref(null)
+</script>
