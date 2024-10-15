81945b30d63cd3b4844224cc7f735df766f6f9e6
Jakub Brambor <kuba.brambor@gmail.com>
2024-10-15 14:17:36 +0200

    create fetcher

---
+++ src/services/useFetchAPI.ts
@@ -0,0 +1,29 @@
+import { ref } from 'vue'
+
+export function useFetchAPI(baseURL: string) {
+  const loading = ref<boolean>(false)
+  const error = ref<string | null>(null)
+
+  const get = async <T>(endpoint: string): Promise<T | null> => {
+    loading.value = true
+    error.value = null
+
+    try {
+      const response = await fetch(`${baseURL}${endpoint}`)
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`)
+      }
+
+      const data = (await response.json()) as T
+      return data
+    } catch (err) {
+      error.value = err.message
+      return null
+    } finally {
+      loading.value = false
+    }
+  }
+
+  return { error, loading, get }
+}
