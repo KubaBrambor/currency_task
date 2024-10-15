2812d5bb179a9ecd355377d221c8921d98909d66
Jakub Brambor <kuba.brambor@gmail.com>
2024-10-15 20:29:30 +0200

    add styling

--- src/App.vue
+++ src/App.vue
@@ -3,77 +3,50 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Currency exchange</RouterLink>
-      </nav>
+  <div class="container">
+    <header class="header">
+      <div class="navWrapper">
+        <nav>
+          <RouterLink to="/" class="link">Currency exchange</RouterLink>
+        </nav>
+      </div>
+    </header>
+    <div class="content">
+      <RouterView />
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+.header {
+  display: flex;
+  justify-content: center;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
   margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.navWrapper nav {
+  display: flex;
+  justify-content: center;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.link {
+  font-weight: bold;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  flex-grow: 1;
 }
 </style>
